d0645cbf967c6bb80e359f45886705e4b8274dd1
Your Name <you@example.com>
2024-09-07 17:57:46 +0300

    second and third ticket

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const process = require("process");
+require("dotenv").config();
+const port = process.env.PORT;
+const url = process.env.URL;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+ mongoose.connect(url).then(() => {
+  console.log("mongo connection started");
+}).catch(err=>{
+  console.log("mongo connection drop")
 })
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+app.use(express.json());
+app.get("/",  (req, res) => {
+  
+ return res.json("hello")
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
