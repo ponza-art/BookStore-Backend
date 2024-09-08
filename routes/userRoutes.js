c7df07df38f481799994c275a6ea3a4e6b311e6d
safaasamir <safaasamirsherif@gmail.com>
2024-09-08 20:04:39 +0300

    userRoute

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,14 @@
 
+const express = require("express");
+const router = express.Router();
+const {getAllUsers,register,login, logout} =require("../controllers/userController");
+const { validateUser, validateLoginUser } = require("../middleware/middelwareUser");
+const verifyToken=require("../middleware/verifyToken")
+router.get("/",verifyToken,getAllUsers)
+router.post("/register",validateUser, register)
+router.post("/login",validateLoginUser,login)
+
+router.post("/logout",logout)
+
+
+module.exports=router
