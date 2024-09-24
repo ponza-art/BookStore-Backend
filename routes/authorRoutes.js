59ef54ed458a3f03c69ebb9fd256b925e092404c
Your Name <you@example.com>
2024-09-24 04:36:16 +0300

    merge of feature/googleLogin

--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -10,22 +10,26 @@ const multer = require("multer");
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
+const verifyAdmin2 = require("../middleware/verifyAdmin2");
+
 
 const router = express.Router();
 
 router.post(
   "/create",
+  verifyAdmin2,
   upload.fields([{ name: "file", maxCount: 1 }]), // Add upload middleware
   createAuthor
 );
 
 router.put(
   "/update/:id",
-  upload.fields([{ name: "file", maxCount: 1 }]), // Add upload middleware
+  verifyAdmin2,
+  upload.fields([{ name: "file", maxCount: 1 }]),
   updateAuthor
 );
 
-router.delete("/delete/:id", deleteAuthor);
+router.delete("/delete/:id", verifyAdmin2,deleteAuthor);
 router.get("/:id", getAuthorById);
 router.get("/", getAllAuthors);
 
