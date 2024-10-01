55960cc01cd60ce4241644b0e8abef6dbe8268e5
Your Name <you@example.com>
2024-10-01 17:19:29 +0300

    final of user status

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const router = express.Router();
-const {getAllUsers,register,login,createAdmin, googleLogin} =require("../controllers/userController");
+const {getAllUsers,register,login,createAdmin, googleLogin, editUserStatus} =require("../controllers/userController");
 const { validateUser, validateLoginUser } = require("../middleware/middelwareUser");
 const verifyAdmin=require("../middleware/verifyAdmin2")
 
@@ -12,7 +12,7 @@ router.post("/register",validateUser, register)
 router.post("/login",validateLoginUser,login)
 router.post("/create-admin",verifyAdmin,createAdmin)
 router.get("/google",googleLogin)
-// router.patch("/edit-status", verifyAdmin, editUserStatus);
+router.patch("/edit-status", verifyAdmin, editUserStatus);
 
 
 module.exports=router
