0cfce984541788efc3b10480610da5a55ce26f96
Your Name <you@example.com>
2024-10-02 03:29:45 +0300

    test of user status

--- middleware/userStatus.js
+++ middleware/userStatus.js
@@ -2,13 +2,13 @@ const AppError = require("../utils/appError");
 
 const checkUserStatus = async (req, res, next) => {
   try {
-    const user = req.user; // Assuming user info is stored in req.user after authentication
+    const user = req.user; 
 
     if (user.status === false) {
       return next(new AppError("User is blocked", 403));
     }
 
-    next(); // User is allowed, proceed
+    
   } catch (error) {
     return next(new AppError("Failed to check user status", 500));
   }
