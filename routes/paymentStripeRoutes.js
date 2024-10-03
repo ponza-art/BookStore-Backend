e39cbf409db50533822b573aa79b02201e5c0dc1
Your Name <you@example.com>
2024-10-03 22:38:35 +0300

    stripe 10

--- routes/paymentStripeRoutes.js
+++ routes/paymentStripeRoutes.js
@@ -3,9 +3,11 @@ const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middleware/verifyToken");
 const { createCheckoutSession, webhook } = require("../controllers/paymentController");
+const bodyParser = require('body-parser');
+
 
 router.post("/create-checkout-session", verifyToken, createCheckoutSession);
-router.post("/webhook", express.raw({ type: "application/json" }), webhook);
+router.post('/webhook', bodyParser.raw({ type: 'application/json' }), webhook);
 
 
 module.exports = router;
