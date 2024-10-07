bfa90b30b5ca9eb400fd5e2767640b0bdab2a8ce
Your Name <you@example.com>
2024-10-07 05:22:15 +0300

    update user status in paymob order

--- routes/paymobRoutes.js
+++ routes/paymobRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { createOrder } = require("../controllers/paymobController");
 const verifyToken = require("../middleware/verifyToken");
+const userStatus = require("../middleware/userStatus");
 
 
-router.post("/",verifyToken,  createOrder);
+
+router.post("/",verifyToken,userStatus,  createOrder);
 // router.get("/", verifyToken, getOrderById);
 // router.delete("/:id", verifyToken, deleteOrder);
 
