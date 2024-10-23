c6a7bddcb51a9eb49207726a6fd856da0bb1dcd4
Smah-amen <samahamen20001@gmail.com>
2024-10-23 21:05:30 +0300

    edit routes contact

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -4,14 +4,16 @@ const {
   getMessages,
   deleteMessage,
 } = require("../controllers/contactController");
+const { createMessageSchema } = require("../validators/contactValidation");
+const validateRequest = require("../middleware/middelewareContact"); 
+
 const verifyAdmin = require("../middleware/verifyAdmin2");
 const verifyUser = require("../middleware/verifyToken");
 
 const router = express.Router();
 
 
-router.post("/", verifyUser, createMessage);
-
+router.post("/", verifyUser, validateRequest(createMessageSchema), createMessage);
 
 router.get("/", verifyAdmin, getMessages);
 router.delete("/:id", verifyAdmin, deleteMessage);
